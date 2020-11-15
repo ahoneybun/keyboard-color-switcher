d4817bcc95fa884c5488f5187442b8009495a7a6
Aaron Honeycutt <aaron@system76.com>
2020-11-15 16:24:49 -0700

    move file around

--- kcc/headerbar.py
+++ kcc/headerbar.py
@@ -1,32 +1,14 @@
 #!/usr/bin/python3
-'''
-   Copyright 2018 Aaron Honeycutt (honeycuttaaron3@gmail.com)
-   This file is part of KCC.
-    KCC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    KCC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with KCC.  If not, see <http://www.gnu.org/licenses/>.
-'''
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from .ppa import PPA
 
-class Headerbar(Gtk.HeaderBar):
-
-    ppa_name = False
+class Headerbar(Gtk.Headerbar):
 
     def __init__(self, parent):
-        self.ppa = PPA(self)
 
-        Gtk.HeaderBar.__init__(self)
+        Gtk.Headerbar.__init__(self)
         self.parent = parent
 
         self.set_show_close_button(True)
@@ -36,6 +18,5 @@ class Headerbar(Gtk.HeaderBar):
         self.switcher.set_baseline_position(Gtk.BaselinePosition.CENTER)
         self.set_custom_title(self.switcher)
 
-        # spinner
         self.spinner = Gtk.Spinner()
         self.pack_end(self.spinner)
--- kcc/stack.py
+++ kcc/stack.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
-'''
-   Copyright 2018 Aaron Honeycutt (honeycuttaaron3@gmail.com)
-   This file is part of KCC.
-    KCC is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    KCC is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with KCC.  If not, see <http://www.gnu.org/licenses/>.
-'''
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
-from .settings import Settings
-from .updates import Updates
-from .list import List
-import gettext
-gettext.bindtextdomain('repoman', '/usr/share/repoman/po')
-gettext.textdomain("repoman")
-_ = gettext.gettext
 
 class Stack(Gtk.Box):
 
@@ -35,12 +14,7 @@ class Stack(Gtk.Box):
         self.stack.set_transition_type(Gtk.StackTransitionType.SLIDE_LEFT_RIGHT)
         self.stack.set_transition_duration(300)
 
-        self.setting = Settings(self)
-        self.updates = Updates(self)
-        self.list_all = List(self)
+        ### Add Labels
 
-        self.stack.add_titled(self.setting, "settings", _("Settings"))
 
         self.pack_start(self.stack, True, True, 0)
-
-
